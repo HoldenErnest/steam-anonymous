d702d127eac7625f9786a63461d08131a44db25a
HoldenErnest <holden.ernest@icloud.com>
2025-04-22 00:45:04 -0400

    good for database

--- src/commands/track.ts
+++ src/commands/track.ts
@@ -7,33 +7,45 @@ dotenv.config();
 const STEAM_KEY = process.env.STEAM_KEY as string;
 
 export const data = new SlashCommandBuilder()
-  .setName("track")
-  .setDescription("Track a user");
+    .setName("track")
+    .setDescription("Setup a user to get tracked")
+    .addUserOption(option => option.setName("user").setDescription('Target user'))
 
 export async function execute(interaction: CommandInteraction) {
     var user = interaction.user.displayName;
-    // TODO: /track @user      keep a db for each user with their ids
+    const targetUser = interaction.options.get("user")?.user?.username;
+
+    if (targetUser) {
+        //TODO: send targetuser to databse
+    }
     createModelForm(interaction);
 }
 
 export async function responseModal(interaction: ModalSubmitInteraction) {
-    interaction.deferReply();
+    await interaction.deferReply();
     var response = ""
     const steamID = interaction.fields.getTextInputValue('steamID_input')
     const steamGame = interaction.fields.getTextInputValue('game_input')
     const steam = new SteamAPI(STEAM_KEY);
-    // TODO: check steam api to find the user. Store this in a database
-    const gameInfo = await getAppId(steamGame, steam);
-    if (gameInfo!.id < 0) {
-        response = `Game '${steamGame}' not found, please try to match a phrase in the title`;
+    // check user
+    const userData = await getSteamUserData(steamID, steam);
+    if (!userData) {
+        response = `User not found with ID: ${steamID}`;
         return interaction.editReply(response);
     }
-    const gameDetails = await checkGame(steamID, gameInfo!.id, steam);
-
+    const usernameString = `[${userData.nickname}](${userData.profileURL})`
+    // get app id from name
+    const gameInfo = await getAppId(steamGame, steam);
+    if (gameInfo!.id < 0) {
+        response = `Game **${steamGame}** not found, please try to match a phrase in the title`;
+        return interaction.editReply(response);
+    }
+    // get details about the game
+    const gameDetails = await userOwnsGame(steamID, gameInfo!.id, steam);
     if (gameDetails) {
-        response = `'${gameInfo?.name}' is now being tracked for user ${steamID}`
+        response = `**${gameInfo?.name}** is now being tracked for ${usernameString}`
     } else {
-        response = `User ${steamID} does not own '${gameInfo?.name}'`;
+        response = `${usernameString} does not own **'${gameInfo?.name}'**`;
     }
 
     return interaction.editReply(response);
@@ -87,7 +99,7 @@ async function getAppId(gameName: string, steam:SteamAPI) {
 }
 
 // returns if the user owns the game, and returns the game info if so.
-async function checkGame(steamID:string, gameID:number, steam:SteamAPI) {
+async function userOwnsGame(steamID:string, gameID:number, steam:SteamAPI) {
     if (!steamID || !gameID) return;
     const res = await steam.getUserOwnedGames(steamID);
 
@@ -96,6 +108,16 @@ async function checkGame(steamID:string, gameID:number, steam:SteamAPI) {
     });
     return result
 }
+async function getSteamUserData(steamID:string, steam:SteamAPI) {
+    if (!Number(steamID)) return false;
+    try {
+        const data = await steam.getUserSummary(steamID);
+        return data;
+    } catch (e){
+        return false;
+    }
+    
+}
 
 function bestDistanceGame(target:string, arr:AppBase[]) {
     var match = {
@@ -103,9 +125,10 @@ function bestDistanceGame(target:string, arr:AppBase[]) {
         id: -1,
         distance: 10 // Change this for minimum distance
     }
+    target = target.toLowerCase()
 
     arr.forEach((game) => {
-        var d = distance(target,game.name);
+        var d = distance(target,game.name.toLowerCase());
         if (d < match.distance) {
             // update the new closest match
             match.distance = d;
